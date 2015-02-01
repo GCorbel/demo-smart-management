7de54d721b42cf672f0993cbb419a45f33e7a3c3
GCorbel <guirec.corbel@gmail.com>
2015-02-01 11:52:08 -0500

    change the root directory

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
       get 'datatables'
     end
   end
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
