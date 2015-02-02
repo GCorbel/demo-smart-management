852b1701874624eefa523e640a92d7ea531a6c1f
GCorbel <guirec.corbel@gmail.com>
2015-02-02 06:19:27 -0500

    Refactor specs

--- spec/models/smart_management/paginer_spec.rb
+++ spec/models/smart_management/paginer_spec.rb
@@ -5,19 +5,34 @@ module SmartManagement
     describe "#call" do
       it 'do the pagination' do
         user1 = User.create(name: 'user1', age: 2)
-        user2 = User.create(name: 'user2', age: 1)
+        User.create(name: 'user2', age: 1)
 
         pagination_options = { start: 0, number: 1 }
         actual = Paginer.new(User.all, pagination_options).call
         expect(actual).to eq [user1]
+      end
+
+      it 'allow to start by a page' do
+        User.create(name: 'user1', age: 2)
+        user2 = User.create(name: 'user2', age: 1)
 
         pagination_options = { start: 1, number: 1 }
         actual = Paginer.new(User.all, pagination_options).call
         expect(actual).to eq [user2]
+      end
+
+      it 'allow to change the number of rows per page' do
+        user1 = User.create(name: 'user1', age: 2)
+        user2 = User.create(name: 'user2', age: 1)
 
         pagination_options = { start: 0, number: 2 }
         actual = Paginer.new(User.all, pagination_options).call
         expect(actual).to eq [user1, user2]
+      end
+
+      it 'allow to star by a page and change the number of row per page' do
+        User.create(name: 'user1', age: 2)
+        user2 = User.create(name: 'user2', age: 1)
 
         pagination_options = { start: 1, number: 2 }
         actual = Paginer.new(User.all, pagination_options).call
--- spec/models/smart_management/sorter_spec.rb
+++ spec/models/smart_management/sorter_spec.rb
@@ -8,7 +8,11 @@ module SmartManagement
         user2 = User.create(name: 'user2', age: 1)
         sort_options = { predicate: :age }
         expect(Sorter.new(User.all, sort_options).call).to eq [user2, user1]
+      end
 
+      it 'can do a reverse sort' do
+        user1 = User.create(name: 'user1', age: 2)
+        user2 = User.create(name: 'user2', age: 1)
         sort_options = { predicate: :age, reverse: true }
         expect(Sorter.new(User.all, sort_options).call).to eq [user1, user2]
       end
